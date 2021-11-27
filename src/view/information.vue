80e7045fa2b878d5fda7cfb8e4351018118f71b8
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-27 14:19:47 +0800

    update form

--- src/view/information.vue
+++ src/view/information.vue
@@ -1,5 +1,24 @@
 <template>
-  <div></div>
+  <van-form @submit="onSubmit">
+    <van-field
+      v-model="username"
+      name="姓名"
+      label="姓名"
+      placeholder="姓名"
+      :rules="[{ required: true, message: '請填寫姓名' }]"
+    />
+    <van-field
+      v-model="password"
+      type="password"
+      name="密碼"
+      label="密碼"
+      placeholder="密碼"
+      :rules="[{ required: true, message: '請填寫密碼' }]"
+    />
+    <div style="margin: 16px">
+      <van-button round block type="info" native-type="submit">提交</van-button>
+    </div>
+  </van-form>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
