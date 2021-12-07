e6087d600c3eefc0e06d4eea11dfbec926542f60
1072030 <46060471+1072030@users.noreply.github.com>
2021-12-07 14:56:06 +0800

    update input frame

--- rent-system-admin/src/view/information.vue
+++ rent-system-admin/src/view/information.vue
@@ -4,7 +4,7 @@
       <div>遊玩日期 :</div>
       <div>遊戲場次 :</div>
       <div>需求人數 :</div>
-      <div>請再次確認您預約的場次→←</div>
+      <div>請再次確認您預約的場次 →←</div>
     </div>
     <van-form @submit="onSubmit">
       <van-field
@@ -29,6 +29,22 @@
         label="E-mail"
         placeholder="E-mail"
         :rules="[{ required: true, message: '請填寫E-mail' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="遊玩人數"
+        label="遊玩人數(3~5人)"
+        placeholder="遊玩人數(3~5人)"
+        :rules="[{ required: true, message: '請填寫遊玩人數' }]"
+      />
+      <van-field
+        v-model="message"
+        rows="1"
+        autosize
+        label="其他問題提問"
+        type="textarea"
+        placeholder="留言輸入"
       />
       <div style="margin: 16px">
         <van-button round block type="info" native-type="submit"
